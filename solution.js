eb8bcc93d9182d1539b8be9ced325fad470deea1
Kush Surti <30488380+FrOz3nFir3@users.noreply.github.com>
2022-11-29 11:44:15 +0530

    changing fallback value

--- solution.js
+++ solution.js
@@ -1,18 +1,18 @@
 // arr = array of numbers
 function major(arr){
   // optimized approach O(N) Time and O(N) Space
-  var ceilNum = Math.floor(arr.length / 2);
+  var floorNum = Math.floor(arr.length / 2);
   var countHt = {};
 
   for(let num of arr){
           if(num in countHt){
                   countHt[num] += 1;
-                  if(countHt[num] > ceilNum) return num;
+                  if(countHt[num] > optimized) return num;
           }else{
                   countHt[num] = 1;
           }
   }      
    // although majority num exisit using this as fallback
-     return -1;  
+     return 1;  
 }
 module.exports=major;
