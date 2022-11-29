5cf9e3bbaa3fd00c0c840d8a72a56f86b67cc420
Kush Surti <30488380+FrOz3nFir3@users.noreply.github.com>
2022-11-29 11:38:53 +0530

    optimized solution

--- solution.js
+++ solution.js
@@ -1,6 +1,18 @@
 // arr = array of numbers
-function major(arr)
-{
-  //your code here
+function major(arr){
+  // optimized approach O(N) Time and O(N) Space
+  var ceilNum = Math.ceil(arr.length / 2);
+  var countHt = {};
+
+  for(let num of arr){
+          if(num in countHt){
+                  countHt[num] += 1;
+                  if(countHt[num] >= ceilNum) return num;
+          }else{
+                  countHt[num] = 1;
+          }
+  }      
+   // although majority num exisit using this as fallback
+     return -1;  
 }
 module.exports=major;
